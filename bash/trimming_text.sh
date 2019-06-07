8b646d1f546178fb9521b79bac9c9f887ec57518
Geoff Lawrence <3411808-glawrence@users.noreply.gitlab.com>
2019-06-07 19:47:41 +0100

    Added text processing bash script

---
+++ bash/trimming_text.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+myvar="Text Processing Demo"
+echo "Value: [$myvar]"
+
+# the -e means process \n as a new line
+echo -e "\nxargs"
+padded="   This is interesting    "
+echo "Before: [$padded]"
+echo "After: [$(echo $padded | xargs)]" # I do not like this as it is not the intended use of xargs
+
+echo -e "\nxargs with newline"
+padded=$'   Line One\nLine Two    '
+echo "Before: [$padded]"
+echo "After: [$(echo $padded | xargs)]"
+
+# the -d of tr means delete characters
+echo -e "\ntr"
+padded=$'   Line One\nLine Two    '
+echo "Before: [$padded]"
+echo "After: [$(echo $padded | tr -d '\n')]"
+
+# awk is the most powerful way to process text
+echo -e "\nawk with newline"
+padded=$'   Line One\nLine Two    '
+echo "Before: [$padded]"
+echo "After: [$(echo $padded | awk '{print $1, $2 " " $3, $4}')]"
