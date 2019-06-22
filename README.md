f2d9748163c145728e4edee6ab7a7e7aa9de9605
Geoff Lawrence <3411808-glawrence@users.noreply.gitlab.com>
2019-06-22 19:03:33 +0100

    Added some Python code

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Geoff Does Stuff
 
-Git repository for example scripts and files to support Geoff Does Stuff. The aim is to provide more complete examples
+Git repository for example scripts and files to support [Geoff Does Stuff](https://www.geoffdoesstuff.com/). The aim is to provide more complete examples than the short snipets on the main site. I hope that by providing full examples it is easier for people to use but also easier for me to test.
---
+++ python/python-info.py
@@ -0,0 +1,9 @@
+import sys
+print("Current Python interpreter is [%s]" % (sys.executable))
+print("Python Version (base):  %s.%s.%s" % sys.version_info[:3])
+print("Python Version (extra): %s.%s.%s %s %s" % (sys.version_info.major, sys.version_info.minor, sys.version_info.micro, sys.version_info.releaselevel, sys.version_info.serial))
+print("Python Version (full):  %s" % (sys.version))
+
+# A quick and easy alternative
+import platform
+print("Python version: %s" % (platform.python_version()))
