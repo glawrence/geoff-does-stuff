3f28929fca8eda5ab5d475478d84834a47dce949
Geoff Lawrence <3411808-glawrence@users.noreply.gitlab.com>
2023-02-13 13:13:28 +0000

    Switched to dependencies

--- windows/windows-sandbox/Install-Winget.ps1
+++ windows/windows-sandbox/Install-Winget.ps1
@@ -5,6 +5,5 @@ Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/download
 Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
 Invoke-WebRequest -Uri https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/2.7.3 -OutFile Microsoft.UI.Xaml.2.7.3.zip
 Expand-Archive -Path Microsoft.UI.Xaml.2.7.3.zip -DestinationPath Microsoft.UI.Xaml.2.7.3 -Force
-Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
-Add-AppxPackage .\Microsoft.UI.Xaml.2.7.3\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.7.appx
-Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+
+Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -DependencyPath Microsoft.VCLibs.x64.14.00.Desktop.appx, .\Microsoft.UI.Xaml.2.7.3\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.7.appx
