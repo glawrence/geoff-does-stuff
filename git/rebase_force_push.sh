f7f4f691c9bf807e847c6e4aa030c00d05cdd447
Geoff Lawrence <3411808-glawrence@users.noreply.gitlab.com>
2025-03-14 18:03:10 +0000

    Update to rebase git script

--- git/rebase_force_push.sh
+++ git/rebase_force_push.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Bash script which works through a number of branches and rebases them on main and then
+# force pushes the changes, so that they are not behind main but up to date
+
 set -eo pipefail
 
 main_branch_name="main" # 'master' is also used
 
 function update_main_branch {
-	echo "~~ update the $main_branch_name branch ~~"
+	echo "Update the $main_branch_name branch"
 	# update the main branch, but without switching to it
-	git fetch origin $main_branch_name:$main_branch_name --recurse-submodules=no --prune
+	git fetch origin $main_branch_name:$main_branch_name --prune
 }
 
 function rebase_branch {
@@ -26,12 +29,26 @@ function rebase_branch {
 
 	# force push
 	echo "~~ pushing changes (force push) ~~"
-	git push --porcelain origin refs/heads/$working_branch:$working_branch --force-with-lease=$working_branch:$latest_commit
+	git push origin refs/heads/$working_branch:$working_branch --force-with-lease=$working_branch:$latest_commit
 
 	echo "Rebasing of $working_branch completed successfully"
 }
 
+# check for changes and don't continue if any found
+echo "Checking for local changes..."
+if [[ ! -z $(git status --short) ]]; then
+	echo "Cannot continue until you commit, disgard or stash your changes"
+	exit 1
+fi
+
 update_main_branch
 
+# capture the current branch
+current_branch=$(git branch --show-current)
+
+# rebase all the branches we need to
 rebase_branch feature/GH-0901_fix-issue
 rebase_branch feature/GH-0907_minor-patch
+
+# switch back to the branch we were on when this script started
+git checkout $current_branch
