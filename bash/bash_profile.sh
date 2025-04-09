8e9ed81a66b3917d25789d722b64802b8ca74c61
Geoff Lawrence <3411808-glawrence@users.noreply.gitlab.com>
2025-04-09 21:06:03 +0100

    Minor addition to bash profile

--- bash/bash_profile.sh
+++ bash/bash_profile.sh
@@ -4,6 +4,7 @@
 
 # clpwd - this could be improved by changing it to a function that detects if dotglob is set or not
 # ntp - this uses a command, ntpdate, that might need installing
+# linetime - on macOS you can install 'hr' and use 'hr - # && date "+%H:%M:%S on %d-%m-%Y" && hr # -'
 
 
 function gjl {
@@ -14,6 +15,7 @@ function gjl {
 	echo
 	echo "  clpwd: this will remove all files, including . files from the current directory"
 	echo "  devclean: remove *.o and *.c files from ~/dev/ path"
+	echo "  linetime: this displays a nice line/section break"
 	echo "  ntp: synchronise time with NTP server (uses sudo)"
 	echo "  powershell: this maps to pwsh which is what actually launches PowerShell Core"
 	echo
@@ -21,6 +23,7 @@ function gjl {
 
 alias clpwd='shopt -s dotglob && rm -fR ./* && shopt -u dotglob'
 alias devclean='find ~/dev/ -name "*.[oc]" -exec rm {}\;'
+alias linetime='echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nDate/Time: $(date +"%T on %e-%b-%Y")\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"'
 alias ntp='sudo ntpdate -u pool.ntp.org'
 alias powershell=pwsh
 
